92c6c46ab5879cdeb06e85569f2de1fb397f6534
Christina Lan <christina.lan13@gmail.com>
2018-11-17 15:27:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -8,7 +8,7 @@ function forLoop(array) {
   }
   return array;
 }
-function whileLoop(array) {
+function whileLoop(n) {
   let countdown = n;
   while (countdown > 0) {
     console.log(--countdown);
